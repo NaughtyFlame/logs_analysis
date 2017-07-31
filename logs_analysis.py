577988a9d76786e17e9055c855dcacbf7b3172e9
SHI <andrewattention@gmail.com>
2017-07-31 21:57:12 +0800

    Update: structure of the code

--- logs_analysis.py
+++ logs_analysis.py
@@ -3,63 +3,86 @@ import psycopg2
 
 DBNAME = "news"
 
+def db_connect():
+    """ Creates and returns a connection to the database defined by DBNAME,
+        as well as a cursor for the database.
 
-def db_execute(query):
+        Returns:
+            db, c - a tuple. The first element is a connection to the database.
+                    The second element is a cursor for the database.
     """
-    connect to datebase, run the query and return the result.
-    Input:
-        query  => SQL query need to run.
-    Output:
-        result => the result returned from datebase
+    try:
+        db = psycopg2.connect(database=DBNAME)
+    except psycopg2.Error as e:
+        print "Can not connect to the database"
+    else:
+        return db
+
+def execute_query(query):
+    """execute_query takes an SQL query as a parameter.
+        Executes the query and returns the results as a list of tuples.
+       args:
+           query - an SQL query statement to be executed.
+
+       returns:
+           A list of tuples containing the results of the query.
     """
-    
-    db = psycopg2.connect(database=DBNAME)
+
+    db = db_connect()
     c = db.cursor()
-    c.execute(query)
+    try:
+        c.execute(query)
+    except psycopg2.Error as e:
+        print "Can not excute the query"
     result = c.fetchall()
     db.close()
     return result
 
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
+    query = """
+    SELECT title, views
+    FROM log_articles
+    ORDER BY views DESC
+    LIMIT 3;
+    """
+    results = execute_query(query)
 
-# Calculate number of access for every article
-query_log_article = """
-SELECT title, views
-FROM log_articles
-ORDER BY views DESC
-LIMIT 3;
-"""
-
-# Calculate number of access for every author
-query_log_author = """
-SELECT name, sum(views) as total
-FROM log_articles, authors
-WHERE author = id
-GROUP BY name
-ORDER BY total DESC;
-"""
-
-# Calculate error rate
-query_log_error = """
-SELECT log_daily.date , count_error::numeric/count_total AS error_rate
-FROM log_daily, log_error
-WHERE log_daily.date = log_error.date
-AND (count_error::numeric/count_total) >= 0.01;
-"""
+    print("\nWhat are the most popular three articles of all time?\n")
+    for title, total in results:
+        print("\"%s\"  --  %s Views" % (title, total))
 
 
-# ----------------------- Main ----------------------- #
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+    query = """
+    SELECT name, sum(views) as total
+    FROM log_articles, authors
+    WHERE author = id
+    GROUP BY name
+    ORDER BY total DESC;
+    """
+    results = execute_query(query)
 
-# Display the report of log analysis of article.
-print("\nWhat are the most popular three articles of all time?\n")
-for title, total in db_execute(query_log_article):
-    print("\"%s\"  --  %s Views" % (title, total))
+    print("\nWho are the most popular article authors of all time?\n")
+    for name, total in results:
+        print("\"%s\"  --  %s Views" % (name, total))
 
-# Display the report of log analysis of author.
-print("\nWho are the most popular article authors of all time?\n")
-for name, total in db_execute(query_log_author):
-    print("\"%s\"  --  %s Views" % (name, total))
+def print_errors_over_one():
+    """Prints out the days where more than 1% of logged access requests were errors."""
+    query = """
+    SELECT log_daily.date , count_error::numeric/count_total AS error_rate
+    FROM log_daily, log_error
+    WHERE log_daily.date = log_error.date
+    AND (count_error::numeric/count_total) >= 0.01;
+    """
+    results = execute_query(query)
 
-# Display the report of log error.
-print("\nOn which days did more than 1% of requests lead to errors?\n")
-for date, rate in db_execute(query_log_error):
-    print("%s  --  %.1f %% errors" % (date, rate*100))
+    print("\nOn which days did more than 1% of requests lead to errors?\n")
+    for date, rate in results:
+        print("%s  --  %.1f %% errors" % (date, rate*100))
+
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
