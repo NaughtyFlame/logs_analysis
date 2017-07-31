23ab84f0f2894c8ec72cf3206e5394d12e71c5c3
SHI <andrewattention@gmail.com>
2017-07-31 21:39:12 +0800

    Update: Query for log error

--- logs_analysis.py
+++ logs_analysis.py
@@ -12,6 +12,7 @@ def db_execute(query):
     Output:
         result => the result returned from datebase
     """
+    
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute(query)
@@ -39,10 +40,10 @@ ORDER BY total DESC;
 
 # Calculate error rate
 query_log_error = """
-SELECT log_daily.date , count_error*1000/count_total AS error_rate
+SELECT log_daily.date , count_error::numeric/count_total AS error_rate
 FROM log_daily, log_error
 WHERE log_daily.date = log_error.date
-AND (count_error*1000/count_total) >= 10;
+AND (count_error::numeric/count_total) >= 0.01;
 """
 
 
@@ -61,4 +62,4 @@ for name, total in db_execute(query_log_author):
 # Display the report of log error.
 print("\nOn which days did more than 1% of requests lead to errors?\n")
 for date, rate in db_execute(query_log_error):
-    print("%s  --  %s %% errors" % (date, rate/10.0))
+    print("%s  --  %.1f %% errors" % (date, rate*100))
